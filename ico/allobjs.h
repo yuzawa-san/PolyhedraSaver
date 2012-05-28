fee94391876f9dc557f375b404c9b674d69d9d03
James Yuzawa <jyuzawa@u.rochester.edu>
2012-05-28 15:02:58 -0400

    initial source commit

---
+++ ico/allobjs.h
@@ -0,0 +1,14 @@
+//
+//  allobjs.h
+//  ico
+//
+//  Created by James Yuzawa on 9/25/11.
+//  Copyright 2011 James Yuzawa. All rights reserved.
+//
+
+// Add headers with definitions of objects here
+
+#include "objcube.h"
+#include "objico.h"
+#include "objocta.h"
+#include "objpyr.h"
