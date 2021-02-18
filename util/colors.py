d3a300886bcf4b19a3a3c39f6e5de1b6a490fe67
James Yuzawa <jtyuzawa@gmail.com>
2021-02-17 19:09:24 -0500

    generate colors

---
+++ util/colors.py
@@ -0,0 +1,17 @@
+import json
+from hsluv import hsluv_to_rgb
+
+# run from root of project:
+# pip3 install -r util/requirements.txt
+# python3 util/colors.py
+
+out = []
+for x in range(0,360):
+    (r,g,b) = hsluv_to_rgb([x, 100, 60])
+    r = int(abs(r) * 255)
+    g = int(abs(g) * 255)
+    b = int(abs(b) * 255)
+    out.append(f"{r:02x}{g:02x}{b:02x}")
+
+with open('Polyhedra/colors.json', 'w') as f:
+    json.dump(out, f)
