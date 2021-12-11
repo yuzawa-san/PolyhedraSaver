a835e8aa5ccade3c668fd797c5e29abb8a0674da
James Yuzawa <jtyuzawa@gmail.com>
2021-12-11 13:18:32 -0500

    move file

--- Polyhedra/PolyhedraScreenSaverView.swift
+++ Polyhedra/PolyhedraScreenSaverView.swift
@@ -81,29 +81,3 @@ class PolyhedraScreenSaverView: ScreenSaverView {
         polyhedronView!.rotate()
     }
 }
-
-class PolyhedronView: NSView {
-    private var radius: CGFloat = .zero
-    private var rotation: Int = .zero
-    private var cachedRenderings: [CachedRendering] = []
-
-    public init(origin: NSPoint, radius: CGFloat, cachedRenderings: [CachedRendering]) {
-        super.init(frame: NSRect(origin: origin, size: NSSize(width: 2 * radius, height: 2 * radius)))
-        self.radius = radius
-        self.cachedRenderings = cachedRenderings
-    }
-
-    @available(*, unavailable)
-    required init?(coder _: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    func rotate() {
-        rotation = (rotation + 1) % cachedRenderings.count
-        needsDisplay = true
-    }
-
-    override func draw(_ dirtyRect: NSRect) {
-        cachedRenderings[rotation].render(position: .zero, radius: radius, lineWidth: 1)
-    }
-}
